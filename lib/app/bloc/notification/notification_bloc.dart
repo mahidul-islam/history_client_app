86da8dbbf6c4d633407a2605a54d34c3f1050e62
mahidul-islam <mizihan84@gmail.com>
2021-07-23 20:20:32 +0600

    some incomplete code

--- lib/app/bloc/notification/notification_bloc.dart
+++ lib/app/bloc/notification/notification_bloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
+// import 'package:meta/meta.dart';
 
 part 'notification_event.dart';
 part 'notification_state.dart';
